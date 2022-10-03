f9a05e71adb95584ac9a4bdfe62b5c764b817660
job van der knaap <job.van.der.knaap@hva.nl>
2022-10-03 14:39:58 +0200

    Started on wemos connection to database.

---
+++ individual-project-docker-main/web/select_user.php
@@ -0,0 +1,32 @@
+<?php
+    require "config.php";
+    $userId;
+    $statusCheck;
+    $insertStatus;
+
+    $checkOut = 0;
+    $checkIn = 1;
+
+    $rfid = $_REQUEST['rfid'];
+    
+    $query = "SELECT `Employee`.employee_id, `CheckIn`.checked_in FROM `Employee` INNER JOIN `CheckIn` ON `Employee`.employee_id = `CheckIn`.employee_id WHERE `Employee`.rfid =" . $rfid . " ORDER BY `CheckIn`.date_time DESC LIMIT 1";
+
+    $result = $conn->query($query);
+    while ($row= $result->fetch_assoc()) {
+        $employeeId = intval($row['employee_id']);
+        $statusCheck = intval($row['checked_in']);
+    }
+
+    if ($statusCheck == $checkOut){
+        $insertStatus = $conn->prepare("INSERT INTO `CheckIn` (checked_in, date_time, employee_id) VALUES (?, NOW(), ?)");
+        $insertStatus->bind_param('ii', $checkIn, $employeeId);
+    } else {
+        $insertStatus = $conn->prepare("INSERT INTO `CheckIn` (checked_in, date_time, employee_id) VALUES (?, NOW(), ?)");
+        $insertStatus->bind_param('ii', $checkOut, $employeeId);
+    }
+
+    $insertStatus->execute();
+      
+    $conn->close();
+
+?>
