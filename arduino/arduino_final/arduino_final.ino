23e8ba16fdbbf054d0b8d93d6e24f051e1f63657
job van der knaap <job.van.der.knaap@hva.nl>
2022-10-04 00:21:39 +0200

    Updated final code for the arduino and updated the wifi test file.

--- arduino/arduino_final/arduino_final.ino
+++ arduino/arduino_final/arduino_final.ino
@@ -2,21 +2,70 @@
 #include <RFID.h>
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
 
-#define SS_PIN 10
+
+#define IN1  2
+#define IN2  3
+#define IN3  4
+#define IN4  5
+#define trigPin 6
+#define echoPin 7
 #define RST_PIN 9
+#define SS_PIN 10
+
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 RFID rfid(SS_PIN,RST_PIN);
 
-int serNum[5];
-int cards[][5] = {{237,131,63,89,8}, {243,90,36,21,152}, {145, 79, 114, 11, 167}};
+int serNums[5];
+String serNum;
+String cards[] = {"237,131,63,89,8", "243,90,36,21,152", "145, 79, 114, 11, 167"};
+const char* ssid = "Knaapjes";
+const char* password = "Amsterdam1";
+String serverName = "http://990ebedadf1e27.lhr.life//select_user.php";
+int distance;
+int bluePin = 2;    //IN1 on the ULN2003 Board, BLUE end of the Blue/Yellow motor coil
+int pinkPin = 3;    //IN2 on the ULN2003 Board, PINK end of the Pink/Orange motor coil
+int yellowPin = 4;  //IN3 on the ULN2003 Board, YELLOW end of the Blue/Yellow motor coil
+int orangePin = 5;  
+int currentStep = 0;
+
+int paso [4][4] =
+{
+  {1, 1, 0, 0},
+  {0, 1, 1, 0},
+  {0, 0, 1, 1},
+  {1, 0, 0, 1}
+};
 
 void setup() {
-  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  pinMode(bluePin, OUTPUT);
+  pinMode(pinkPin, OUTPUT);
+  pinMode(yellowPin, OUTPUT);
+  pinMode(orangePin,OUTPUT);
+  
   lcd.begin();
   SPI.begin();
   rfid.init();
+
+  Serial.begin(9600);
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
 
   lcd.backlight();
   setBeginScreen();
@@ -24,50 +73,65 @@ void setup() {
 }
 
 void loop() {  
-  if(rfid.isCard()) {        
-    if(rfid.readCardSerial()) {       
-      for(int i = 0; i < sizeof(rfid.serNum); i++ ) {
-        if(rfid.serNum[i] == cards[0][i]) {
-          lcd.clear();
-          lcd.setCursor(0,0);
-          lcd.print("Hi! job");
-          lcd.setCursor(0,1);
-          lcd.print("enjoy work today");
+  if (WiFi.status()== WL_CONNECTED){
+    WiFiClient client;
+    HTTPClient http;
 
-          delay(1500);
+    for (int i=0; i<sizeof(serNums); i++){
+        serNum = serNum + String(serNums[i]);
+      }
+      
+    if(rfid.isCard()) {        
+      if(rfid.readCardSerial()) {        
+        if(serNum == cards[0]) {
+          Serial.write(serNum);
+          setWelcomeScreen("Job");
+          delay(500);
+          do {
+            distance = getDistance();
+          } while (distance > 25);
+
+          turnGate();
           setBeginScreen();
           break;
-        } else if(rfid.serNum[i] == cards[1][i]) {
-          lcd.clear();
-          lcd.setCursor(0,0);
-          lcd.print("Hi! mats");
-          lcd.setCursor(0,1);
-          lcd.print("enjoy work today");
+          
+        } else if(serNum == cards[1]) {
+          Serial.write(serNum);
+          setWelcomeScreen("Mats");
+          delay(500);
+          do {
+            distance = getDistance();
+          } while (distance > 25);
 
-          delay(1500);
+          turnGate();
           setBeginScreen();
           break;
-        } else if(rfid.serNum[i] == cards[2][i]) {
-          lcd.clear();
-          lcd.setCursor(0,0);
-          lcd.print("Hi! OV");
-          lcd.setCursor(0,1);
-          lcd.print("enjoy work today");
+          
+        } else if(serNum == cards[2]) {
+          Serial.write(serNum);
+          setWelcomeScreen("OV");
+          delay(500);
+          do {
+            distance = getDistance();
+          } while (distance > 25);
 
-          delay(1500);
+          turnGate();
           setBeginScreen();
           break;
+          
         } else {
-          setErrorScreen();
+          setTryScreen();
           delay(1500);
           setBeginScreen();
           break;
         }
       }
     }
+    delay(500);
+    rfid.halt();  
+  } else {
+    setErrorScreen();
   }
-  delay(500);
-  rfid.halt();  
 }
 
 void setBeginScreen(){
@@ -78,10 +142,64 @@ void setBeginScreen(){
   lcd.print("To digital works");
 }
 
-void setErrorScreen(){
+void setTryScreen(){
   lcd.clear();
   lcd.setCursor(3,0);
   lcd.print("Please try");
   lcd.setCursor(5,1);
   lcd.print("again!");
+}
+
+void setWelcomeScreen(String name){
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Hi! " + name);
+  lcd.setCursor(0,1);
+  lcd.print("enjoy work today");
+}
+
+void setErrorScreen(){
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("No wifi conn...");
+  lcd.setCursor(0,1);
+  lcd.print("get assistance!");
+}
+
+int getDistance(){
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  long duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  int distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+  // Displays the distance on the Serial Monitor
+  return distance;
+}
+
+void turnGate(){
+  digitalWrite(bluePin, LOW);
+  digitalWrite(pinkPin, LOW);
+  digitalWrite(yellowPin, LOW);
+  digitalWrite(orangePin, LOW);
+
+  unsigned long starttime = millis();
+  unsigned long endtime = starttime;
+  
+  while((endtime - starttime) <=1500) // do this loop for up to 1000mS
+  {
+    for (int i = 0; i < 4; i++)
+    {
+      digitalWrite(IN1, paso[i][0]);
+      digitalWrite(IN2, paso[i][1]);
+      digitalWrite(IN3, paso[i][2]);
+      digitalWrite(IN4, paso[i][3]);
+      delayMicroseconds(2250);
+    }
+    endtime = millis();
+  }
 }
--- arduino/wifi.ino/wifi.ino.ino
+++ arduino/wifi.ino/wifi.ino.ino
@@ -2,24 +2,57 @@
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
 
+const char* ssid = "Knaapjes";
+const char* password = "Amsterdam1";
+
+String serverName = "http://990ebedadf1e27.lhr.life//select_user.php";
+unsigned long lastTime = 0;
+unsigned long timerDelay = 20000;
+
 void setup() {
-  WiFi.begin("iotroam", "goQYjjfhMu");
+  Serial.begin(9600); 
 
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH); // Making sure the LED is off!
-
-  // Keep in while-loop while the device is not connected to your accesspoint.
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000); // Waiting on connection...
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
   }
-  String rfid = "243,90,36,21,152";
-  WiFiClient client;
-  HTTPClient httpClient;
-
-  httpClient.begin(client, "https://9072283bcb1826.lhrtunnel.link/select_user.php?rfid=" + rfid);
-  httpClient.GET();
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  
+  // Send an HTTP POST request depending on timerDelay
+  if ((millis() - lastTime) > timerDelay) {
+    //Check WiFi connection status
+    if(WiFi.status()== WL_CONNECTED){
+      WiFiClient client;
+      HTTPClient http;
+
+      String serverPath = serverName + "?rfid=237,131,63,89,8";
+      
+      // Your Domain name with URL path or IP address with path
+      http.begin(client, serverPath.c_str());
+      int httpResponseCode = http.GET();
+      
+      if (httpResponseCode>0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+        String payload = http.getString();
+        Serial.println(payload);
+      }
+      else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+      }
+      // Free resources
+      http.end();
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
+  }
 }
