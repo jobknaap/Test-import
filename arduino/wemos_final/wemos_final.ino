92a2c2f9050e3a110a2c68ef7a4cc5842484a28e
job van der knaap <job.van.der.knaap@hva.nl>
2022-10-11 16:11:46 +0200

    Finalized code by adding comments.

--- arduino/wemos_final/wemos_final.ino
+++ arduino/wemos_final/wemos_final.ino
@@ -1,28 +1,40 @@
+/*
+  @author: Job van der Knaap
+  @license: Permissive
+  Goal: The goal of this code is to receive data from the arduino and sent this to the http server
+*/
+
+// Libraries
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
 
-const char* ssid = "iotroam";
-const char* password = "goQYjjfhMu";
-String serverName = "http://60a8524b02f5a6.lhr.life/select_user.php?rfid=";
+// Constant variables
+#define SSID = "iotroam"
+#define PASSWORD = "goQYjjfhMu"
+#define SERVER_NAME = "http://60a8524b02f5a6.lhr.life/select_user.php?rfid="
 
 void setup() {
+  // Sarting serial and wifi
   Serial.begin(9600);
-  WiFi.begin(ssid, password);
+  WiFi.begin(SSID, PASSWORD);
 
-  // Stopping 
+  // Delaying code untill wifi is connected
   while(WiFi.status() != WL_CONNECTED) {
     delay(500);
   }
 }
 
 void loop() {
+  // Constructing client and http
   WiFiClient client;
   HTTPClient http;
+
+  // Checks if serial is sent from other device
   if(Serial.available()>0){ 
-    String rfid = Serial.readString(); //"237,131,63,89,8";   // It will read the incoming or arriving data byte  
     
-    String serverPath = serverName + rfid;
+    String rfid = Serial.readString(); // It will read the incoming string sent from the wemos 
+    String serverPath = serverName + rfid; // Creates the full server path for the http
     
     // Your Domain name with URL path or IP address with path
     http.begin(client, serverPath.c_str());
